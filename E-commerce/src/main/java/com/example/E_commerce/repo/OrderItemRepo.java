4bd94bf7f4dbc29820ae08687a89ac774fedc715
Sandesh adhikari <sandesh12adk@gmail.com>
2025-05-18 08:09:05 +0545

    @Repository

--- E-commerce/src/main/java/com/example/E_commerce/repo/OrderItemRepo.java
+++ E-commerce/src/main/java/com/example/E_commerce/repo/OrderItemRepo.java
@@ -2,6 +2,8 @@ package com.example.E_commerce.repo;
 
 import com.example.E_commerce.entity.OrderItem;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderItemRepo extends PagingAndSortingRepository<OrderItem,Integer>  {
 }
--- E-commerce/src/main/java/com/example/E_commerce/repo/UserRepo.java
+++ E-commerce/src/main/java/com/example/E_commerce/repo/UserRepo.java
@@ -3,6 +3,8 @@ package com.example.E_commerce.repo;
 import com.example.E_commerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.jmx.export.naming.IdentityNamingStrategy;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
 }
